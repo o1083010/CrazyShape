fed998ae7e24f4ca7b9064f86df94df98fed41e8
o1083010 <o1083010@gm.pu.edu.tw>
2021-05-24 10:32:42 +0800

    2021上機考v2_(王琪雯)

--- app/src/main/java/tw/edu/pu/o10830100/crazyshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/o10830100/crazyshape/MainActivity.kt
@@ -4,10 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
 import kotlinx.android.synthetic.main.activity_main.*
 
 
+
+@GlideModule
+public final class MyAppGlideModule : AppGlideModule()
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +24,11 @@ class MainActivity : AppCompatActivity() {
 
         Toast.makeText(this,"作者:王琪雯",Toast.LENGTH_LONG).show()
 
+        val img: ImageView = findViewById(R.id.imgTitle)
+        GlideApp.with(this)
+            .load(R.drawable.cover)
+            .override(800, 600)
+            .into(img)
 
 
         imgNext.setOnLongClickListener (object: View.OnLongClickListener{
