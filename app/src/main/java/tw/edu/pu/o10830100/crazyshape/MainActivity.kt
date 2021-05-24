fa57ed30caad6a5ce00629797961a3449fffe030
o1083010 <o1083010@gm.pu.edu.tw>
2021-05-24 13:01:46 +0800

    2021上機考v3_(王琪雯)

--- app/src/main/java/tw/edu/pu/o10830100/crazyshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/o10830100/crazyshape/MainActivity.kt
@@ -1,6 +1,7 @@
 package tw.edu.pu.o10830100.crazyshape
 
 import android.content.Intent
+import android.os.Build.VERSION_CODES.N
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,7 +10,7 @@ import android.widget.Toast
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.module.AppGlideModule
 import kotlinx.android.synthetic.main.activity_main.*
-
+import java.util.stream.DoubleStream.generate
 
 
 @GlideModule
@@ -18,28 +19,52 @@ public final class MyAppGlideModule : AppGlideModule()
 
 class MainActivity : AppCompatActivity() {
 
+    var X: Int = (1..N).random()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Toast.makeText(this,"作者:王琪雯",Toast.LENGTH_LONG).show()
+        val shape =
+            intArrayOf(R.drawable.circle, R.drawable.square, R.drawable.star, R.drawable.triangle)
+        val i: Int = (0..3).random()
+        imgNext.setImageResource(shape[i])
+
+        Toast.makeText(this, "作者:王琪雯", Toast.LENGTH_LONG).show()
 
         val img: ImageView = findViewById(R.id.imgTitle)
+
         GlideApp.with(this)
             .load(R.drawable.cover)
             .override(800, 600)
             .into(img)
 
 
-        imgNext.setOnLongClickListener (object: View.OnLongClickListener{
+        imgNext.setOnLongClickListener(object : View.OnLongClickListener {
             override fun onLongClick(v: View?): Boolean {
 
                 intent = Intent(this@MainActivity, GameActivity::class.java)
                 startActivity(intent)
+                recreate()
 
                 return true
             }
         })
 
+
+
+        imgNext.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val shape =
+                    intArrayOf(R.drawable.circle, R.drawable.square, R.drawable.star, R.drawable.triangle)
+                val i: Int = (0..3).random()
+                imgNext.setImageResource(shape[i])
+                imgNext.setImageResource(shape[i])
+
+                recreate()
+
+
+            }
+        })
     }
 }
